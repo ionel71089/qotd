901e12b09fed5207d5d37e7d3bedfb01608ea6da
Lescai, Ionel <ionel.lescai@flir.com>
2021-01-29 12:25:22 +0200

    bump script

---
+++ scripts/bump.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+podspec-bump -w
+version=`podspec-bump --dump-version`
+git add qotd.podspec
+git commit -m "Release ${version}"
+git tag $version
+git push origin master --tags
